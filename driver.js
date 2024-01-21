6c477452988a1d3e86a461a6460a1075870e8601
Ari <52634803+theblackmace@users.noreply.github.com>
2024-01-21 22:22:30 +0530

    Shit I fucked up, this one will be the second and last commit frfr

---
+++ driver.js
@@ -0,0 +1,15 @@
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight) - 48;
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
+
+
+document.getElementById('yesButton').addEventListener("click", nextPage);
+document.getElementById('noButton').addEventListener("mouseover", moveButton);
+document.getElementById('noButton').addEventListener("click", moveButton);
